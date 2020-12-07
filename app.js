83c39f4675509aa61118cbc02f18ecf3891b4fab
thiblehm12 <66329321+thiblehm12@users.noreply.github.com>
2020-12-07 08:49:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function toggleSidebar(){
+    document.getElementById("navbar").classList.toggle('active');
+};
+
+$("#slide").click( function() {
+    $(".icon").toggleClass("close");
+});
+function redirect() {
+    window.location = "https://google.com";
+ }
